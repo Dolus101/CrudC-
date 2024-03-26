67459571fa1297e3cc5d9bda179bb3ffd8b78c49
unknown <puppeteer102@gmail.com>
2024-03-26 13:42:53 +0800

    Fix Upload/Create

--- obj/Debug/net7.0/DizonCoop.AssemblyInfo.cs
+++ obj/Debug/net7.0/DizonCoop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DizonCoop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2555eb820be51dee1ebeed185bba8c6b4cad891f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05b4abf4790ee104043586aba46e9e1c9e7db8a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DizonCoop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DizonCoop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
